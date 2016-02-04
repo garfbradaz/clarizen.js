72511847e06bcde866afc967e2ec36eb7a5bb5ae
Gareth Bradley <garfbradaz@outlook.com>
2016-02-04 16:49:30 +0000

    first commiet to dev

--- clarizen.js
+++ clarizen.js
@@ -1,1 +1,34 @@
-clarizen.js
+var Clarizen = function (userName, password, applicationId, optionalSessionId) {
+
+    if (userName === undefined) {
+        throw new Error('userName is missing');
+    }
+    if (password === undefined) {
+        throw new Error('password is missing');
+    }
+    this.userName = userName;
+    this.password = password;
+    this.applicationId = applicationId;
+    this.sessionId = (optionalSessionId === undefined) ? '' : optionalSessionId;
+};
+
+Clarizen.prototype.Login = function() {
+    console.log("un : " + this.userName);
+};
+
+
+var Person = function (firstName) {
+  this.firstName = firstName;
+};
+
+Person.prototype.sayHello = function () {
+    console.log("Hello, I'm " + this.firstName);
+};
+
+var person1 = new Person("Alice");
+var person2 = new Person("Bob");
+
+// call the Person sayHello method.
+person1.sayHello(); // logs "Hello, I'm Alice"
+person2.sayHello(); // logs "Hello, I'm Bob"
+
